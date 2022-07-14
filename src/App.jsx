d209f919f4690876c5c77e4d10c104d35e166a11
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-07-14 11:47:20 +0200

    Implement KPI for number of orders
    Implementation of the responsive box containing the KPI for the number of orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import Header from "./components/header/Header";
+import OrdersKPI from "./components/ordersKPI/OrdersKPI";
+import "./App.scss";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="kpis-container">
+        <OrdersKPI />
+      </div>
+    </>
+  );
 }
 
 export default App;
