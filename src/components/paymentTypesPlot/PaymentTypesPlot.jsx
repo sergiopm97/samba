3d011edb39dcfda61f3b509f52c15e757329a0b7
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-07-26 01:35:08 +0200

    Implement responsive version of the chart
    Implementation of the responsive version of the payment type distribution chart

--- src/components/paymentTypesPlot/PaymentTypesPlot.jsx
+++ src/components/paymentTypesPlot/PaymentTypesPlot.jsx
@@ -1,5 +1,5 @@
 import payments from "../../data/payments.json";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import "./PaymentTypesPlot.scss";
 import colors from "../colors";
 
@@ -9,13 +9,15 @@ function PaymentTypesPlot() {
   return (
     <div className="payment-types-plot-container">
       <h1 className="payment-types-plot-container--title">Payment types distribution</h1>
-      <BarChart width={600} height={300} data={paymentTypes}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip cursor={{ fill: colors.mediumBlue, opacity: 0.75 }} content={<CustomToolTip />} />
-        <Bar dataKey="Count" fill={colors.pink} />
-        <XAxis dataKey="Payment type" />
-        <YAxis />
-      </BarChart>
+      <ResponsiveContainer width={"90%"} height={"80%"}>
+        <BarChart data={paymentTypes}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip cursor={{ fill: colors.mediumBlue, opacity: 0.75 }} content={<CustomToolTip />} />
+          <Bar dataKey="Count" fill={colors.pink} />
+          <XAxis dataKey="Payment type" />
+          <YAxis />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
